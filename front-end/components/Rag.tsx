ac0f72ae2d4e028826004f4fe755beb6fd556406
Adam Hatim <adamhatim123@gmail.com>
2025-10-16 18:41:18 +0200

    netifly for front end

--- front-end/components/Rag.tsx
+++ front-end/components/Rag.tsx
@@ -25,7 +25,7 @@ const Rag: React.FC = () => {
   setLoading(true);
 
   try {
-    const res = await fetch("http://127.0.0.1:8000/chat", {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/chat`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ query: userMessage }),
