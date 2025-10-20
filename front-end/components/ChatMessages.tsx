e2574b17ec8f929aeecbecdad077a12e0542be76
Adam Hatim <adamhatim123@gmail.com>
2025-10-20 14:11:23 +0200

    --

--- front-end/components/ChatMessages.tsx
+++ front-end/components/ChatMessages.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export interface ChatMessageType {
   userRequest: string;
@@ -13,20 +15,22 @@ interface ChatMessagesProps {
 
 const ChatMessages: React.FC<ChatMessagesProps> = ({ messages }) => {
   return (
-    <div className="flex flex-col space-y-3">
+    <div className="flex flex-col space-y-4">
       {messages.map((msg, index) => (
         <div key={index} className="flex flex-col space-y-2">
-          {/* User */}
+          {/* User message */}
           <div className="flex justify-end">
-            <div className="max-w-[70%] rounded-xl bg-blue-600 text-white p-2.5 text-sm shadow-md break-words">
-              <p>{msg.userRequest}</p>
+            <div className="max-w-[70%] rounded-2xl bg-blue-600 text-white p-3 text-sm shadow-md break-words whitespace-pre-wrap">
+              {msg.userRequest}
             </div>
           </div>
 
-          {/* Bot */}
+          {/* Bot message (Markdown) */}
           <div className="flex justify-start">
-            <div className="max-w-[70%] rounded-xl bg-gray-200 text-gray-800 p-2.5 text-sm shadow-sm break-words">
-              <p>{msg.botResponse}</p>
+            <div className="max-w-[75%] rounded-2xl bg-gray-100 text-gray-900 p-3 text-sm shadow-sm break-words prose prose-sm max-w-none">
+              <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                {msg.botResponse}
+              </ReactMarkdown>
             </div>
           </div>
         </div>
